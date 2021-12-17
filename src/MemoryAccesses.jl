987d62a5f8653dd6068957aa31070e9095334e97
Takafumi Arakaki <aka.tkf@gmail.com>
2021-12-16 19:11:10 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.23

---
+++ src/MemoryAccesses.jl
@@ -0,0 +1,11 @@
+baremodule MemoryAccesses
+
+module Internal
+
+using ..MemoryAccesses: MemoryAccesses
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule MemoryAccesses
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MemoryAccesses
+using Test
+
+@testset "MemoryAccesses.jl" begin
+    # Write your tests here.
+end
